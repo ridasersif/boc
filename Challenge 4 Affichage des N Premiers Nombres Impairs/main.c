e938613bd369683da8fd2c7500709e1ff47991aa
ridasersif <ridasersif1@gmail.com>
2024-09-25 18:07:24 +0200

    v4

---
+++ Challenge 4 Affichage des N Premiers Nombres Impairs/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N,i,imp;
+    printf("veuillez entere un nombre\N=");
+    scanf("%d",&N);
+
+    for (i=0;i<(N/2);i++)
+    {
+        imp=
+    }
+
+
+
+    return 0;
+}
